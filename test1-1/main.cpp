9e4482c6bc43ca5a9c6ee0382da04173eaecf84a
王超 <wangchao10@bianfeng.com>
2017-11-09 17:28:21 +0800

    添加项目文件。

---
+++ test1-1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Cpoint {
+public:
+	static int value;
+	static int num;
+
+	~Cpoint() { num++; cout << "µ÷ÓÃÎö¹¹:" << num << endl; }
+
+	void print(int i) {
+		cout << "this is int:" << i << endl;
+	}
+
+	void print(double i) {
+		cout << "this is double:" << i << endl;
+	}
+
+private:
+	int xp, yp;
+};
+
+int Cpoint::value = 0;
+int Cpoint::num = 0;
+
+
+int main()
+{
+	Cpoint c;
+	int a = 10;
+	double b = 10.00;
+	c.print(b);
+}
