5a22b5294439a751b95e75e34765e3f203530ae1
王超 <wangchao10@bianfeng.com>
2017-11-09 19:58:27 +0800

    test oparetor

--- test1-1/main.cpp
+++ test1-1/main.cpp
@@ -5,15 +5,32 @@ class Cpoint {
 public:
 	static int value;
 	static int num;
-
+	Cpoint(int x, int y) :xp(x),yp(y){}
 	~Cpoint() { num++; cout << "µ÷ÓÃÎö¹¹:" << num << endl; }
 
 	void print(int i) {
 		cout << "this is int:" << i << endl;
 	}
 
-	void print(double i) {
+	int print(double i) {
 		cout << "this is double:" << i << endl;
+		int a = 1111;
+		return a;
+	}
+
+	Cpoint operator/(const Cpoint& a) {
+		Cpoint point(0,0);
+		point.xp = this->xp + a.xp;
+		point.yp = this->yp + a.yp;
+		return point;
+	}
+
+	int showx() {
+		return xp;
+	}
+
+	int showy() {
+		return yp;
 	}
 
 private:
@@ -26,8 +43,8 @@ int Cpoint::num = 0;
 //test
 int main()
 {
-	Cpoint c;
-	int a = 10;
-	double b = 10.00;
-	c.print(b);
+	Cpoint c(1,2);
+	Cpoint e(3,4);
+	Cpoint d = c /e;
+	cout << "this is test:"  << d.showy() << endl;
 }
